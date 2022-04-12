88255421783c78deccd627ef4db61baf0288263a
mgoetze <99mg24@web.de>
2022-04-12 19:43:21 +0200

    todo added

--- py_tools/transform.py
+++ py_tools/transform.py
@@ -3,6 +3,8 @@ from lxml import etree #https://lxml.de/validation.html
 xslt = etree.parse("tools_html.xsl") #parse xsl file
 xml = etree.parse("tools.xml") #parse xml file
 #this is a change
+
+#TODO: Transform with parameters --> z.B. rot oder grüne schriftfarbe https://stackoverflow.com/questions/35595940/example-for-setting-multiple-parameters-in-python-lxml-xslt
 transform = etree.XSLT(xslt)
 newdom = transform(xml)
 print(etree.tostring(newdom, pretty_print=True))
