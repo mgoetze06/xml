d9d8e187ecf7b828bafd55edebfc52472af4a0a3
mgoetze <99mg24@web.de>
2022-04-13 11:14:16 +0200

    parameter from user input

--- py_tools/transform.py
+++ py_tools/transform.py
@@ -5,23 +5,25 @@ xslt_par = etree.parse("tools_html_param.xsl")
 xml = etree.parse("tools.xml") #parse xml file
 #this is a change
 
-#TODO: Transform with parameters --> z.B. rot oder grüne schriftfarbe https://stackoverflow.com/questions/35595940/example-for-setting-multiple-parameters-in-python-lxml-xslt
+#Transform with parameters --> z.B. rot oder grüne schriftfarbe https://stackoverflow.com/questions/35595940/example-for-setting-multiple-parameters-in-python-lxml-xslt
 def transformToHtml():
     transform = etree.XSLT(xslt)
     newdom = transform(xml)
     print(etree.tostring(newdom, pretty_print=True))
     infile = (etree.tostring(newdom, pretty_print=True)).decode("utf-8")
-    outfile = open("result_html.html", 'a')
+    outfile = open("result_html.html", 'w+')
     outfile.write(infile)
 
 def transformToHtmlParam():
     transform = etree.XSLT(xslt_par)
     #lom_tree = xslt(original_tree, var1=etree.XSLT.strparam("str_example"), var2=etree.XSLT.strparam(var))
-    newdom = transform(xml, var1=etree.XSLT.strparam("red"))
+    text = input("Input headline for xml doc: ")
+    newdom = transform(xml, var1=etree.XSLT.strparam(text))
     print(etree.tostring(newdom, pretty_print=True))
     infile = (etree.tostring(newdom, pretty_print=True)).decode("utf-8")
-    outfile = open("result_html.html", 'w+')
+    outfile = open("result_html_param.html", 'w+')
     outfile.write(infile)
 
 
 transformToHtmlParam()
+transformToHtml()
